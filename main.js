61df252de884821d2ba372f5629f9dfe15f841d4
rajeeve12 <rajeevevishen21@gmal.com>
2024-10-20 18:53:57 -0400

    Handle stock availability and disable purchase button when out of stock

---
+++ main.js
@@ -0,0 +1,24 @@
+//Task 1 is in html file
+
+//Task 2
+const size = 0;
+
+function dropdown() {
+    for (let size = 0; index < array.length; index++) {
+        const element = array[index];
+        if (size>0) {
+            return dropdown;
+        }
+    }
+}
+
+//Task 3
+if (size<dropdown) {
+    for (let index = 0; index < array.length; index++) {
+        const size = array[index];
+        
+    }
+    console.log("Out of Stock");
+}
+
+//Task 4
